b7125bd5d54fcc129f5a0bc9e78c06f830d0a0b5
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2020-10-14 11:34:57 +0800

    升级 squirrel core 依赖到 0.2.69

--- utils/squirrel.js
+++ utils/squirrel.js
@@ -94,7 +94,7 @@ window.scriptExecutor = (script, text, paramsJson) => {
   return result ? JSON.stringify(result) : '';
 };
 
-const SquirrelUtools = require('./squirrel-utools-0.1.62')
+const SquirrelUtools = require('./squirrel-utools-0.1.69')
 const Squirrel = SquirrelUtools.com.lanyuanxiaoyao.squirrel.utools
 const SquirrelWrapper = {
   info: () => JSON.parse(Squirrel.info('default')),
