6255cf3a207e874469059416ff740ac4a775a8cc
lanyuanxiaoyao <lanyuanxiaoyao@gmail.com>
2020-11-03 11:38:55 +0800

    使 plugin.json 跟随 package.json 的相关信息

--- build.js
+++ build.js
@@ -1,8 +1,15 @@
-const pluginConfigTemplate = {
+const fs = require('fs')
+
+let packageConfig = JSON.parse(fs.readFileSync('./package.json'))
+let version = packageConfig['version']
+let description = packageConfig['description']
+let author = packageConfig['author']
+
+let pluginConfigTemplate = {
   'pluginName': '资源搜索 Lite',
-  'version': '0.1.0',
-  'description': '海纳百川, 搜遍天下, 轻量版的资源搜索',
-  'author': 'lanyuanxiaoyao',
+  'version': version,
+  'description': description,
+  'author': author,
   'homepage': 'http://lanyuanxiaoyao.com',
   'logo': 'logo.png',
   'preload': 'preload.js',
@@ -17,9 +24,6 @@ const isEmpty = require('licia/isEmpty')
 const startWith = require('licia/startWith')
 
 const defaultIcon = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAAkFBMVEUAAAA/RFFESlM6P0g5P0c5PUU9Qkk5QEg7QkpARlFBRk48P0ZES1X///9xxP9mvv9AR09KU13R7P90zv+x3/+Z1f+K0P9ko9Fduv/l9f/d8f/Iys1OaYHp6uvf4OLNz9FXXWXs9//G5/+kp6tvdX32+/+p2/+Byf+ws7dekbd4fYO+5P9ksOhhi6u+x82CuOK007hMAAAADXRSTlMAaBnQ7bqumok9DtoolNF8hgAAAYRJREFUOMuVUwmSgjAQ3Mgt5gQCAooCouCx///dTghodK2y7KI4qic9TWfy8w2WAfI9z0fB8i29QI7N+G7Hme2gxX8eWaxtiu1msy2aloXodbnLhgLPEGnJ3CeRlcVP+AERkZ5bK2O9xStsoKMkSrn10HBNfn1OCAAq3Ls/ZugLQikhcEU9Q1ODcDDkkwyWHyUBlOFiEqjMArVerImyoSWWTosN1JRQifGZAkpHZRrYjab2dQ73A/RQT9F1ydUOxg46ofxIiVB1Z+A1bmMPn//KWLvLOvyELfehwOOXTII0CND6uWCz83SBkhZgPsbvCnx+kfr3TF7EYm6BWBrFeQz57aes61wkJIPPYjQZ2NeIjJDafpypqGmCcWMHY1Al0aBkPSrIDPgjvLcQ1NRD8XeX+wTCBL5iaNqsu8RsU9QHuA9qs7REryWowAZODD0GZmxC5cHgK+4aIwcVLwKVGjlzaHsSmUmd9NCaY1+mVM50MeixN4FCVl5v88Gx0Kej9+nwfoE/h9gtUp11ZxUAAAAASUVORK5CYII='
-
-const fs = require('fs')
-const path = require('path')
 
 const sites = require('./sites')
 sites
